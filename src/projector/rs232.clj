cc2d8d7acf3d28ab342fd34cf41f50c9c2db22b8
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-03-22 19:37:53 -0400

    Update travis configuration, issue #3

--- src/projector/rs232.clj
+++ src/projector/rs232.clj
@@ -1,5 +1,5 @@
 (ns projector.rs232
-  (:require [serial.core :refer [open close! write]])
+  ;(:require [serial.core :refer [open close! write]])
   (:import (projector.device Device)))
 
 (def dummy-port "dummy")
@@ -9,10 +9,12 @@
   (connect [this])
   (disconnect [this])
   (transmit [this command] (if (not= port dummy-port)
-                             (do (write port command) command)
+                             (do command command)
                              command)))
-
+;[clj-serial "2.0.4-SNAPSHOT"]
+;(write port command)
+;(open port)
 (defn create-a-connection
   [port]
-  (let [serial-port (if (not= port dummy-port) (open port) dummy-port)]
+  (let [serial-port (if (not= port dummy-port) port dummy-port)]
     (RS232Projector. serial-port)))
