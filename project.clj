d2abe0960add4c26ba41b14eca6f79c364623a28
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-03-22 23:51:42 -0400

    Adding coveralls integration

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/ieer/projector"
   :license {:name "GNU General Public License v3.0"
             :url  "https://github.com/ieer/projector/blob/master/LICENSE"}
+  :plugins [[lein-cloverage "1.0.9"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-serial "2.0.3"]]
   :target-path "target/%s"
